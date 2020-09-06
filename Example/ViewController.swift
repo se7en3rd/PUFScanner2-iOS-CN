d4674ca9d84f7d05e96f5b995962f975f6df6e92
se7en3rd <se7en3rd@linxtelecom.co.kr>
2020-09-06 16:27:44 +0900

    Initial Commit

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by se7en3rd on 2020/09/06.
+//  Copyright © 2020 Linx Telecom Co.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
